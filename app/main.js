8dd5e5d5b3e53ed968897279897bc67d8121d077
Roi Levy <roilevi65xxx@gmail.com>
2019-05-30 15:31:22 +0300

    Redesigned again

--- app/main.js
+++ app/main.js
@@ -3,28 +3,27 @@ const url = require('url');
 const path = require('path');
 var exec = require('child_process').exec;
 
-const {app,BrowserWindow,Menu,ipcMain} = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
 let calWindow;
 let status = false;
 
-app.on('ready',function(){
+app.on('ready', function() {
     mainWindow = new BrowserWindow({
-        webPreferences: {nodeIntegration: true},
-        titleBarStyle: 'hiddenInset' 
-});
+        webPreferences: { nodeIntegration: true },
+        titleBarStyle: 'hiddenInset',
+    });
     dir = exec("cd ../ && make", function(err, stdout, stderr) {
         console.log(stdout);
-      });
+    });
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname,"index.html"),
-        protocol:'file:',
+        pathname: path.join(__dirname, "index.html"),
+        protocol: 'file:',
         slashes: true
     }));
-
-    mainWindow.on('closed',function(){
-        dir = exec("ps -ef | grep ../gl | grep -v grep |  xargs kill ", function(err, stdout, stderr) {console.log(stderr);});
+    mainWindow.on('closed', function() {
+        dir = exec("ps -ef | grep ./gl | grep -v grep | awk '{print $2}'  | xargs kill ", function(err, stdout, stderr) { console.log(stderr); });
         app.quit();
     });
 
@@ -33,26 +32,28 @@ app.on('ready',function(){
 });
 
 //Open Calibration
-function size(){
+function size() {
     calWindow = new BrowserWindow({
-            webPreferences: {nodeIntegration: true},
-        width:700,
-        height:300,
-        titleBarStyle: 'hiddenInset' 
+        webPreferences: { nodeIntegration: true },
+        width: 700,
+        resizable: false,
+        fullscreen: false,
+        height: 300,
+        titleBarStyle: 'hiddenInset'
     });
     calWindow.loadURL(url.format({
-        pathname: path.join(__dirname,"size.html"),
-        protocol:'file:',
+        pathname: path.join(__dirname, "size.html"),
+        protocol: 'file:',
         slashes: true
     }));
-    calWindow.on('closed',function(){
+    calWindow.on('closed', function() {
         calWindow = null;
 
     })
 }
 
 
-ipcMain.on('close',function(e,item){
+ipcMain.on('close', function(e, item) {
     if (!status)
         dir = exec("../gl c " + item, function(err, stdout, stderr) {});
     else
@@ -61,18 +62,29 @@ ipcMain.on('close',function(e,item){
     calWindow.close();
 });
 
-const mainMenuTem = [
-    {
-        label : 'Program',
-        submenu:[
-            {
-                label:'Settings',
+ipcMain.on('marker', function(e, item) {
+    dir = exec("ps -ef | grep ./gl | grep -v grep | awk '{print $2}'  | xargs kill ", function(err, stdout, stderr) { console.log(stderr); });
+    status = true;
+    size();
+});
+
+ipcMain.on('calibration', function(e, item) {
+    dir = exec("ps -ef | grep ./gl | grep -v grep | awk '{print $2}'  | xargs kill ", function(err, stdout, stderr) { console.log(stderr); });
+    status = false;
+    size();
+});
+
+
+const mainMenuTem = [{
+        label: 'Program',
+        submenu: [{
+                label: 'Settings',
                 accelerator: process.platform == 'darwin' ? 'Cmd+,' : 'Ctrl+,',
             },
             {
-                label:'Developer',
+                label: 'Developer',
                 accelerator: process.platform == 'darwin' ? 'Cmd+I' : 'Ctrl+I',
-                click(item,focusedWindow){
+                click(item, focusedWindow) {
                     focusedWindow.toggleDevTools();
                 }
 
@@ -81,30 +93,30 @@ const mainMenuTem = [
         ]
     },
     {
-        label:'File',
-        submenu:[
-            {
-                label:'Calibration',
+        label: 'File',
+        submenu: [{
+                label: 'Calibration',
                 accelerator: process.platform == 'darwin' ? 'Option+C' : 'Alt+C',
-                click(){
-                    dir = exec("ps -ef | grep ../gl | grep -v grep | xargs kill ", function(err, stdout, stderr) {console.log(stderr);});
+                click() {
+                    dir = exec("ps -ef | grep ./gl | grep -v grep | awk '{print $2}'  | xargs kill ", function(err, stdout, stderr) { console.log(stderr); });
                     status = false;
                     size();
                 }
             },
             {
-                label:'Run',
+                label: 'Run',
                 accelerator: process.platform == 'darwin' ? 'Option+R' : 'Alt+R',
-                click(){
-                    dir = exec("ps -ef | grep ../gl | grep -v grep | xargs kill ", function(err, stdout, stderr) {console.log(stderr);});
+                click() {
+                    dir = exec("ps -ef | grep ./gl | grep -v grep | awk '{print $2}'  | xargs kill ", function(err, stdout, stderr) { console.log(stderr); });
                     status = true;
                     size();
                 }
             },
             {
-                label:'Quit',
+                label: 'Quit',
                 accelerator: process.platform == 'darwin' ? 'Cmd+Q' : 'Ctrl+Q',
-                click(){
+                click() {
+                    dir = exec("ps -ef | grep ./gl | grep -v grep | awk '{print $2}'  | xargs kill ", function(err, stdout, stderr) { console.log(stderr); });
                     app.quit();
                 }
             }
